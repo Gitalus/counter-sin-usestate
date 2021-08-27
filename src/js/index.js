3d684e049d3c6bbc15c0f0971243c2929ec765c7
Gitalus <italog.penaloza@gmail.com>
2021-08-27 21:56:08 +0000

    add button with behaviors

--- src/js/index.js
+++ src/js/index.js
@@ -11,14 +11,38 @@ import "../styles/index.scss";
 //import your own components
 import { SecondsCounter } from "./component/SecondsCounter";
 
+let intervalo = setInterval(counterIncrement, 1000);
+
 let counter = 0;
 
 function counterIncrement() {
 	ReactDOM.render(
-		<SecondsCounter counter={counter} />,
+		<>
+			<SecondsCounter counter={counter} />
+			<button onClick={restartInterval}>Resume</button>
+			<button onClick={stopInterval}>Stop</button>
+			<form onSubmit={countDown}>
+				<input
+					type="number"
+					placeholder="Enter a CountDown"
+					name="inputCountdown"></input>
+				<button type="submit">Countdown</button>
+			</form>
+		</>,
 		document.querySelector("#app")
 	);
 	counter++;
 }
 
-setInterval(counterIncrement, 1000);
+function restartInterval() {
+	intervalo = setInterval(counterIncrement, 1000);
+}
+
+function stopInterval() {
+	clearInterval(intervalo);
+}
+
+function countDown(event) {
+	event.preventDefault();
+	counter = Number(event.target.inputCountdown.value);
+}
